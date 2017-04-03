0c97145cdc2556910b8434c4349707178da39071
ikneb <ikneblis@gmail.com>
2017-04-03 18:30:40 +0300

    create theme for wc(shop,cart,account...)

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php /*if ( is_active_sidebar( 'maxstore-footer-area' ) ) { */?><!--
+    <div id="content-footer-section" class="row clearfix">
+        <?php /*dynamic_sidebar( 'maxstore-footer-area' ) */?>
+    </div>
+--><?php /*} */?>
+<footer id="colophon" class="rsrc-footer" role="contentinfo">
+    <div class="row rsrc-author-credits">
+        <div class="text-center">
+            <span class="sep"> | </span>
+        </div>
+    </div>
+</footer>
+<div id="back-top">
+    <a href="#top">
+        <span></span>
+    </a>
+</div>
+</div>
+<!-- end main container -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(trailingslashit( get_template_directory() ) . 'lib/wp_bootstrap_navwalker.php');
+
+function my_theme_stylesheets() {
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '3.3.4', 'all' );
+    wp_enqueue_style( 'mytheme-stylesheet', get_stylesheet_uri(), array(), '1.3.7', 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'my_theme_stylesheets' );
+
+if (wc_get_page_id( 'cart' ) == get_the_ID()) {
+   var_dump(get_the_ID());exit;
+}
